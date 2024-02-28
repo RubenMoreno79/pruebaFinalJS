00487269dfd58cc77b01431728202f5e7e4aeaf7
Ruben_Moreno <rubenm.olivar@gmail.com>
2024-02-28 09:17:47 +0100

    HMTL y CSS empezado

---
+++ data.js
@@ -0,0 +1,17 @@
+const listaTareas = [
+    {
+        idTarea: '0',
+        Titulo: 'Estudiar JavaScript',
+        Prioridad: 'Urgente',
+    },
+    {
+        idTarea: '2',
+        Titulo: 'Dormir',
+        Prioridad: 'Diaria',
+    },
+    {
+        idTarea: '3',
+        Titulo: 'Salir a comer',
+        Prioridad: 'Mensual',
+    },
+];
